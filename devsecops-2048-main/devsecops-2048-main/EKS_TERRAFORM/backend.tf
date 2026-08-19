db51be5b6b4f5915df1ff0463e7200d03bde62e8
sanjeevanvarma <sanjeevanvarma42@gmail.com>
2026-08-19 10:48:00 +0530

    Delete devsecops-2048-main/devsecops-2048-main directory

--- devsecops-2048-main/devsecops-2048-main/EKS_TERRAFORM/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "devsecops-2048-reyaz-4pm" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
-  }
-}
