0c56decefa019c4682f0fed9fe347cadbc25dc81
sanjeevanvarma <sanjeevanvarma42@gmail.com>
2026-08-19 11:06:28 +0530

    Updated backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-2048-reyaz-4pm" # Replace with your actual S3 bucket name
+    bucket = "devsecops-2048-game-sanju-19-26"
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
